b91c6372a74031778c64a25a687a7659b716aadf
gitamgadtaula <gitamgadtaula@gmail.com>
2021-04-24 20:29:19 +0545

    latest changes

--- src/App.vue
+++ src/App.vue
@@ -3,37 +3,50 @@
     <!-- <p>list items like this</p> -->
     <div class="filters">
       <div class="filters_title">
-    <button @click="toggleIndustry">Industry<img class="chevron-up" src="./assets/up-chevron.svg" ></button>
-    <button @click="toggleSalary">Salary<img class="chevron-up" src="./assets/up-chevron.svg" ></button>
-    <button @click="toggleExperience">Experience<img class="chevron-up" src="./assets/up-chevron.svg" ></button>
-    <button @click="toggleCompany">Company<img class="chevron-up" src="./assets/up-chevron.svg" ></button>
-    <button @click="toggleOthers">Others<img class="chevron-up" src="./assets/up-chevron.svg" ></button>
+        <div class="filter-item">
+          <button @click="toggleIndustry">
+            Industry <img class="chevron-up" :src="arrow(showIndustry)" />
+          </button>
+          <industry-filter
+            :show="showIndustry"
+            @industry-filter="getIndustryFilter"
+          />
+        </div>
+        <div class="filter-item">
+          <button @click="toggleSalary">
+            Salary<img class="chevron-up" :src="arrow(showSalary)" />
+          </button>
+          <salary-filter :show="showSalary" @salary-filter="getSalaryFilter" />
+        </div>
+        <div class="filter-item">
+          <button @click="toggleExperience">
+            Experience<img class="chevron-up" :src="arrow(showExperience)" />
+          </button>
+          <experience-filter
+            :show="showExperience"
+            @experience-filter="getExperienceFilter"
+          />
+        </div>
+        <div class="filter-item">
+          <button @click="toggleCompany">
+            Company<img class="chevron-up" :src="arrow(showCompany)" />
+          </button>
+          <company-filter
+            :show="showCompany"
+            @company-filter="getCompanyFilter"
+          />
+        </div>
+        <div class="filter-item">
+          <button @click="toggleOthers">
+            Others<img class="chevron-up" :src="arrow(showOthers)" />
+          </button>
+          <others-filter :show="showOthers" @others-filter="getOthersFilter" />
+        </div>
       </div>
-      <div class="action_buttons">
+      <!-- <div class="action_buttons">
       <button class="clear">Clear all</button>
       <button class="apply" >Apply(128)</button>
-      </div>
-    <industry-filter
-      :show="showIndustry"
-      @industry-filter="getIndustryFilter"
-    />
-    <salary-filter
-      :show="showSalary"
-      @salary-filter="getSalaryFilter"
-    />
-    <experience-filter
-    :show="showExperience"
-    @experience-filter="getExperienceFilter"
-    />
-    <company-filter
-    :show="showCompany"
-    @company-filter="getCompanyFilter"
-    />
-    <others-filter
-    :show="showOthers"
-    @others-filter="getOthersFilter"
-    />
-    
+      </div> -->
     </div>
   </div>
 </template>
@@ -42,11 +55,17 @@
 import IndustryFilter from "./components/IndustryFilter.vue";
 import SalaryFilter from "./components/SalaryFilter.vue";
 import ExperienceFilter from "./components/ExperienceFilter.vue";
-import CompanyFilter from "./components/CompanyFilter.vue"
-import OthersFilter from "./components/OthersFilter.vue"
+import CompanyFilter from "./components/CompanyFilter.vue";
+import OthersFilter from "./components/OthersFilter.vue";
 export default {
   name: "App",
-  components: { IndustryFilter, SalaryFilter, ExperienceFilter,CompanyFilter, OthersFilter },
+  components: {
+    IndustryFilter,
+    SalaryFilter,
+    ExperienceFilter,
+    CompanyFilter,
+    OthersFilter,
+  },
   data() {
     return {
       showIndustry: false,
@@ -57,19 +76,25 @@ export default {
     };
   },
   methods: {
+    arrow(value) {
+      if (value) {
+        return require("./assets/up-chevron.svg");
+      }
+      return require("./assets/down-arrow.png");
+    },
     toggleIndustry() {
       this.showIndustry = !this.showIndustry;
     },
-    toggleSalary(){
+    toggleSalary() {
       this.showSalary = !this.showSalary;
     },
-    toggleExperience(){
+    toggleExperience() {
       this.showExperience = !this.showExperience;
     },
-    toggleCompany(){
+    toggleCompany() {
       this.showCompany = !this.showCompany;
     },
-     toggleOthers(){
+    toggleOthers() {
       this.showOthers = !this.showOthers;
     },
     getIndustryFilter(e) {
@@ -80,7 +105,7 @@ export default {
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap');
-@import url('./assets/css/style.css');
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap");
+@import url("./assets/css/style.css");
 </style>
--- src/components/ExperienceFilter.vue
+++ src/components/ExperienceFilter.vue
@@ -1,27 +1,24 @@
 <template>
-<div class="all_cards">
-  <div v-if="show">
-    <div class="card">
-      <!-- <div class="search_company">
-        <input type="text" placeholder="Search company">
-        <img  src="../assets/search.svg">
-        </div> -->
-      <ul>  
-        <li v-for="(item, index) in options" :key="index">
-          <label class="container">
-          <input type="checkbox" v-model="form[item.value]" />
-          <span class="checkmark"></span>
-          <span class="title">{{ item.title }}</span> <span class="count">({{ item.count }})</span>
-          </label>
-        </li>
-      </ul>
-      <div class="card_buttons">
-      <button class="clear" @click="clear">Clear all</button>
-      <button class="apply" @click="emitValue">Apply</button>
+  <div class="all_cards">
+    <div v-if="show">
+      <div class="card">
+        <ul>
+          <li v-for="(item, index) in options" :key="index">
+            <label class="container">
+              <input type="checkbox" v-model="form[item.value]" />
+              <span class="checkmark"></span>
+              <span class="title">{{ item.title }}</span>
+              <span class="count">({{ item.count }})</span>
+            </label>
+          </li>
+        </ul>
+        <div class="card_buttons">
+          <button class="clear" @click="clear">Clear all</button>
+          <button class="apply" @click="emitValue">Apply</button>
+        </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -40,7 +37,6 @@ export default {
         { title: "3 - 5 Years", count: 1023, value: "3-5_years" },
         { title: "5 - 10 Years", count: 1023, value: "5-10_years" },
         { title: "Above 10 Years", count: 1023, value: "above10years" },
-            
       ],
       form: {},
     };
@@ -57,8 +53,7 @@ export default {
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-ul li:first-child{
-  padding-top:0;
+ul li:first-child {
+  padding-top: 0;
 }
-
 </style>
